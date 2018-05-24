c590b79746466db98e02e0022473968407bd28b6
InsomniaDev <adam9190@gmail.com>
2018-05-24 09:35:30 -0600

    Functionality changes
    Adds the change for all output to be written to log file and rearchitects the utilities packages

--- server/main.go
+++ server/main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+
+	"./pkg/utilities"
 )
 
 func main() {
-	fmt.Println("Something will happen here soon")
+
+	// Testing out functionality...
+
+	// This changes all output to go to a log file rather than a console
+	utilities.AssignLogFile()
+
+	// Tests out the timer functionality
+	for x := 0; x < 10; x++ {
+		utilities.SecondSleeper()
+		fmt.Println(strconv.Itoa(x+1) + " Timer has slept for a second")
+	}
 }
